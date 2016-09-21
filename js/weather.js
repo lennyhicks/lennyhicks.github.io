11be870002d6ae8ac251341902eed288fd3f8993
Lenny Hicks <lenny.hicks@interapthq.com>
2016-09-21 12:57:40 -0400

    Commit

--- js/weather.js
+++ js/weather.js
@@ -106,3 +106,12 @@ function getWeather() {
     });
 
 }
+
+function jsonCallback(json) {
+    console.log(json);
+}
+
+$.ajax({
+    url: "https://crossorigin.me/http://dataservice.accuweather.com/currentconditions/v1/" + locationKey + ".json?language=en&apikey=" + apiKey + "&details=true&callback=?",
+    dataType: "jsonp"
+});
